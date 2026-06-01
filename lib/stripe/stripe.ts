41cf152188657d0e830a7280fb46a4388787e94c
sunloveandlight <sunloveandlight@gmail.com>
2026-06-01 12:19:57 -0700

    f

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-04-22.dahlia",
+  apiVersion: "2026-05-27.dahlia",
 });
 
 // ✅ keep old import working
