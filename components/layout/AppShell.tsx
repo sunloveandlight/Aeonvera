33466414b5d06679c9232eed238cd3a2ecc7fbc2
sunloveandlight <sunloveandlight@gmail.com>
2026-06-05 16:43:27 -0700

    f

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -7,9 +9,9 @@ type AppShellProps = {
 
 export default function AppShell({ children }: AppShellProps) {
   return (
-    <div className="min-h-screen flex flex-col relative">
+    <main className="min-h-screen bg-black text-white flex flex-col antialiased">
 
-      {/* GLOBAL BACKGROUND (SYSTEM-WIDE) */}
+      {/* GLOBAL BACKGROUND */}
       <div className="fixed inset-0 -z-10">
         <div className="absolute inset-0 bg-black" />
 
@@ -18,16 +20,17 @@ export default function AppShell({ children }: AppShellProps) {
         <div className="absolute inset-0 bg-[linear-gradient(to_bottom,transparent,rgba(255,255,255,0.02))]" />
       </div>
 
-      {/* HEADER */}
+      {/* HEADER (SINGLE SOURCE OF TRUTH) */}
       <Header />
 
-      {/* PAGE CONTENT WRAPPER (IMPORTANT CONSISTENCY LAYER) */}
-      <main className="flex-1 w-full">
+      {/* PAGE CONTENT */}
+      <div className="flex-1">
         {children}
-      </main>
+      </div>
 
       {/* FOOTER */}
       <Footer />
-    </div>
+
+    </main>
   );
 }
