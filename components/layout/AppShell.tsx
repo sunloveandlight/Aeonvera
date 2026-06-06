ab5134eab24178e1f17776589bc920ef59076056
sunloveandlight <sunloveandlight@gmail.com>
2026-06-05 17:20:27 -0700

    f

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -9,18 +9,9 @@ type AppShellProps = {
 
 export default function AppShell({ children }: AppShellProps) {
   return (
-    <main className="min-h-screen bg-black text-white flex flex-col antialiased">
+    <main className="min-h-screen flex flex-col antialiased">
 
-      {/* GLOBAL BACKGROUND */}
-      <div className="fixed inset-0 -z-10">
-        <div className="absolute inset-0 bg-black" />
-
-        <div className="absolute inset-0 bg-[radial-gradient(circle_at_top,rgba(255,255,255,0.08),transparent_45%)]" />
-
-        <div className="absolute inset-0 bg-[linear-gradient(to_bottom,transparent,rgba(255,255,255,0.02))]" />
-      </div>
-
-      {/* HEADER (SINGLE SOURCE OF TRUTH) */}
+      {/* HEADER */}
       <Header />
 
       {/* PAGE CONTENT */}
