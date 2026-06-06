52ab6ef582d97134338be1a8dceeaaec76717903
sunloveandlight <sunloveandlight@gmail.com>
2026-06-05 18:04:13 -0700

    f

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -2,17 +2,34 @@
 
 import Header from "./Header";
 import Footer from "./Footer";
+import { useOverlayMode } from "@/lib/audit/useOverlayMode";
 
 type AppShellProps = {
   children: React.ReactNode;
 };
 
 export default function AppShell({ children }: AppShellProps) {
-  return (
-    <main className="min-h-screen flex flex-col antialiased">
+  const overlayEnabled = useOverlayMode();
 
+  return (
+    <main
+      className="
+        min-h-screen
+        flex flex-col
+        antialiased
+        relative
+      "
+      data-aeonvera-app
+    >
       {/* HEADER */}
       <Header />
+
+      {/* SYSTEM INDICATOR (DEV ONLY) */}
+      {overlayEnabled && (
+        <div className="fixed bottom-4 right-4 z-[9999] px-3 py-1 rounded-full bg-white text-black text-xs">
+          AEONVERA OVERLAY
+        </div>
+      )}
 
       {/* PAGE CONTENT */}
       <div className="flex-1">
@@ -21,7 +38,6 @@ export default function AppShell({ children }: AppShellProps) {
 
       {/* FOOTER */}
       <Footer />
-
     </main>
   );
 }
