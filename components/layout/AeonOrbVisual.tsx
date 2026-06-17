118be14c3e5eb79a1dedbcd12537ca02d518111b
sunloveandlight <sunloveandlight@gmail.com>
2026-06-17 07:47:09 -0700

    f

--- components/layout/AeonOrbVisual.tsx
+++ components/layout/AeonOrbVisual.tsx
@@ -7,14 +7,6 @@ type AeonOrbVisualProps = {
   energy?: AeonOrbEnergy;
 };
 
-const RINGS = [
-  "aeon-orb-ring-gold",
-  "aeon-orb-ring-teal",
-  "aeon-orb-ring-violet",
-  "aeon-orb-ring-white",
-  "aeon-orb-ring-soft",
-];
-
 const PARTICLES = Array.from({ length: 14 }, (_, index) => index);
 
 export default function AeonOrbVisual({ className = "", energy = "idle" }: AeonOrbVisualProps) {
@@ -32,9 +24,13 @@ export default function AeonOrbVisual({ className = "", energy = "idle" }: AeonO
         ))}
       </span>
       <span className="aeon-orb-rings">
-        {RINGS.map((ring) => (
-          <span key={ring} className={`aeon-orb-ring ${ring}`} />
-        ))}
+        <svg className="aeon-orb-plasma-field" viewBox="-100 -100 200 200" focusable="false">
+          <path className="aeon-orb-plasma-arc aeon-orb-plasma-gold" d="M -50 6 C -30 -34 28 -36 50 -6" />
+          <path className="aeon-orb-plasma-arc aeon-orb-plasma-teal" d="M -44 -14 C -22 36 24 38 46 10" />
+          <path className="aeon-orb-plasma-arc aeon-orb-plasma-violet" d="M -38 38 C -54 2 -16 -38 40 -38" />
+          <path className="aeon-orb-plasma-arc aeon-orb-plasma-white" d="M -42 -34 C 0 -48 48 -18 42 30" />
+          <path className="aeon-orb-plasma-arc aeon-orb-plasma-soft" d="M -54 18 C -18 52 40 36 56 -14" />
+        </svg>
       </span>
       <span className="aeon-orb-core" />
     </span>
